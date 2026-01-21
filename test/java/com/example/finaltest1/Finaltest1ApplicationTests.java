c8eb8a9c44df4ad13c5dced37db56f7dc901129c
Sami Zerfaoui <121903761+TheRealChickon@users.noreply.github.com>
2026-01-21 10:02:00 +0100

    Add files via upload

---
+++ test/java/com/example/finaltest1/Finaltest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finaltest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Finaltest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
