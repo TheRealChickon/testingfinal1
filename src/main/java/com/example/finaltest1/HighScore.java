c5307402812279e191ea76179dc70a62fc757742
Sami Zerfaoui <121903761+TheRealChickon@users.noreply.github.com>
2026-02-22 20:22:39 +0100

    done

--- src/main/java/com/example/finaltest1/HighScore.java
+++ src/main/java/com/example/finaltest1/HighScore.java
@@ -18,11 +18,6 @@ public class HighScore {
 
     private String name;
 
-
-
-
-
-
     // Getters and Setters
     public String getName() {return name;}
     public void setName(String name) {this.name = name;}
