acb2dc15523babe279f766e88bdefb969a571d42
Sami Zerfaoui <121903761+TheRealChickon@users.noreply.github.com>
2026-03-10 15:50:14 +0100

    sorting 2.0

--- src/main/java/com/example/finaltest1/HighScoreRepository.java
+++ src/main/java/com/example/finaltest1/HighScoreRepository.java
@@ -1,6 +1,7 @@
 package com.example.finaltest1;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,9 @@ public interface HighScoreRepository extends JpaRepository<HighScore, Long> {
 
     public Optional<HighScore> findByName(String name);
 
-    List<HighScore> findTop6ByOrderByScoreDesc();
+    @Override
+    @Query("SELECT h FROM HighScore h ORDER BY h.score DESC")
+    public List<HighScore> findAll();
+
+    // List<HighScore> findTop6ByOrderByScoreDesc();
 }
