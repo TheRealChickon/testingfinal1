438c515a46b21d8a2791d6ffdf16a7e8be406ef5
Sami Zerfaoui <121903761+TheRealChickon@users.noreply.github.com>
2026-03-11 11:54:44 +0100

    override 3

--- src/main/java/com/example/finaltest1/HighScoreRepository.java
+++ src/main/java/com/example/finaltest1/HighScoreRepository.java
@@ -1,9 +1,11 @@
 package com.example.finaltest1;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,9 +14,19 @@ public interface HighScoreRepository extends JpaRepository<HighScore, Long> {
 
     public Optional<HighScore> findByName(String name);
 
-    // @Override
-    @Query("SELECT h FROM HighScore h ORDER BY h.score DESC")
-    public List<HighScore> findAll();
+//    @Override
+//    @Query("SELECT h FROM HighScore h ORDER BY h.score DESC")
+//    public List<HighScore> findAll();
 
     // List<HighScore> findTop6ByOrderByScoreDesc();
+
+
+    @Override
+    default List<HighScore> findAll() {
+        return findAll(Sort.unsorted()).stream()
+                .sorted(Comparator.comparing(HighScore::getScore).reversed())
+                .limit(6)
+                .toList();
+    }
+
 }
